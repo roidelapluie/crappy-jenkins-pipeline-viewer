bb58eede1b79e2800bccddbf4ed7b315fb3f8880
Julien Pivotto <roidelapluie@inuits.eu>
2013-10-24 17:24:36 +0200

    add support for disabled job and git commit

--- check-pipeline.py
+++ check-pipeline.py
@@ -27,16 +27,27 @@ def show_job(job):
     job = j.get_job_info(job)
     color = job['color']
     attr = None
+    on_color = None
     if re.search('.*_.*', color):
         colors = color.split('_')
         color = colors[0]
         attr = ["blink"]
+    if color == 'disabled':
+        color = 'white'
+        on_color = 'on_grey'
     if color == 'blue':
         color = 'green'
     build_number = ""
     if len(job['builds']) > 0:
         build_number = job['builds'][0]['number']
-    print colored(job['name'], color, None, attr), build_number
+    print "\033[K" + colored(job['name'], color, on_color, attr), build_number,
+    try:
+        for change in j.get_build_info(job['name'],build_number)['changeSet']['items']:
+            print change['author']['fullName'],
+            print change['commitId'][:8],
+    except:
+        pass
+    print ""
     if job.has_key('downstreamProjects') and len(job['downstreamProjects']) > 0:
         show_job(job['downstreamProjects'][0]['name'])
 
